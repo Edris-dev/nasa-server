9251cbb68bd0ccc45a03cee7a9fb245e6c61286c
Edris-dev <75594567+Edris-dev@users.noreply.github.com>
2022-01-18 11:17:54 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');//middleware module
+const cors = require('cors');//allow fetch module
+const bodyParser = require('body-parser');
+
+//static DB
+let db = [];
+let app = express();//will use express middleware
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/userInfo', getDb);
+app.post('/update', updateDb);
+app.post('/remove', fixDb);
+
+function fixDb(req,res,err){
+  let delCard = req.body.removedCard;
+
+  if(!(delCard ===  undefined)){
+    let newDb = db.filter(({id}) => id !== delCard[0].id);
+    db = newDb;
+  }
+}
+
+function getDb(req,res,err){
+  res.status(200).json(db);
+}
+
+function updateDb(req,res,err){
+
+  if(req.body.userInfo.length > 0){
+    db = req.body.userInfo;
+    res.status(200).json(db);
+  }else{
+      res.status(200)
+  }
+
+}
+
+app.listen(3005, () =>{
+  console.log("Server listening at http://localhost:3005");
+});
