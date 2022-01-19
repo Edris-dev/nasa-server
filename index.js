31ce29148f0f0e1ba7260c30337d95bc83333c49
Edris-dev <75594567+Edris-dev@users.noreply.github.com>
2022-01-19 01:55:01 -0500

    update DB

--- index.js
+++ index.js
@@ -12,36 +12,63 @@ app.use(bodyParser.json());
 
 app.get('/', (req,res) => {res.send("working!")});
 app.get('/userInfo', getDb);
-app.post('/update', updateDb);
-app.post('/remove', fixDb);
+app.post('/newEntry', addList);
+app.post('/removeEntry', cutList);
+app.post('/lateComment', comList);
 
+function cutList(req,res,next){
 
+  if(req.body.oldCard === undefined){
+    res.status(404).send("Bad entry");
+  }else{
+    let oldId = req.body.oldCard.id;
+    const updatedDb = db.filter(({id}) => id !== oldId);
+    db = updatedDb;
+    res.status(200).json(db)
+  }
 
-function fixDb(req,res,err){
-  let delCard = req.body.removedCard;
+}
 
-  if(!(delCard ===  undefined)){
-    let newDb = db.filter(({id}) => id !== delCard[0].id);
-    db = newDb;
+function addList(req,res,next){
+
+  if(req.body.newCard === undefined){
+    res.status(404).send("Bad entry");
+  }else{
+    db.push(req.body.newCard)
+    res.status(200).json(db)
+
   }
 }
+
+function comList(req,res,next){
+
+  if(req.body.upCard === undefined){
+    res.status(404).send("Bad entry");
+  }else{
+    let upId = req.body.upCard.id;
+    db.map((photo) => {
+      if(photo.id === upId){
+        photo.comment = req.body.userComm;
+      }
+    })
+    res.status(200).json(db)
+
+  }
+}
+
 
 function getDb(req,res,err){
   res.status(200).json(db);
 }
 
-function updateDb(req,res,err){
-
-  if(req.body.userInfo.length > 0){
-    db = req.body.userInfo;
-    res.status(200).json(db);
-  }else{
-      res.status(200)
-  }
-
-}
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () =>{
   console.log(`Server is listening on port ${PORT}` );
 });
+
+/*console.log("inside Adder on DB side")
+console.log("what i recieved was ");
+console.log(req.body.newCard);
+console.log("or")
+console.log(req.body)*/
